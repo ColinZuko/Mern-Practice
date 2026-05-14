924b9f332a97ceb69049fc3233e6dee44580a4fe
Colin Delsink <164153350+ColinZuko@users.noreply.github.com>
2026-05-14 22:53:22 +0200

    server.js changes

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,14 @@ const multer = require('multer');
 const upload = multer();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(upload.any());
 app.use(posts);
@@ -19,7 +24,7 @@ app.use(users);
 app.use(awsRoutes);
 
 
-app.listen(PORT, () =>  {
+app.listen(PORT, '0.0.0.0', () => {
     connect.connectToServer();
     console.log(`Server is running on port: ${PORT}`);
-})
+});
