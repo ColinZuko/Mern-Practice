53e2083e7328a4cfbd88cb16d6f524351f9aa39a
Colin Delsink <164153350+ColinZuko@users.noreply.github.com>
2026-07-03 10:45:21 +0200

    Updated backend for Vercel Hosting

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-const connect = require('./connect');
-const express = require('express');
-const cors = require('cors');
-const posts = require('./postRoutes');
-const users = require('./userRoutes');
-const awsRoutes= require('./awsRoutes');
-const multer = require('multer');
+require("dotenv").config();
+const connect = require("./connect");
+const express = require("express");
+const cors = require("cors");
+const posts = require("./postRoutes");
+const users = require("./userRoutes");
+const awsRoutes = require("./awsRoutes");
+const multer = require("multer");
 const upload = multer();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
@@ -23,8 +23,4 @@ app.use(posts);
 app.use(users);
 app.use(awsRoutes);
 
-
-app.listen(PORT, '0.0.0.0', () => {
-    connect.connectToServer();
-    console.log(`Server is running on port: ${PORT}`);
-});
+module.exports = app;
